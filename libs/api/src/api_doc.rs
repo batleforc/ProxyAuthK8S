a4a4ba079244a95c4614548d57014a4a18d2d8bb
Max Batleforc <maxleriche.60@gmail.com>
2025-09-21 16:22:31 +0200

    fix(utoipa): automate the generation of Swagger api

--- libs/api/src/api_doc.rs
+++ libs/api/src/api_doc.rs
@@ -3,5 +3,8 @@ use utoipa::OpenApi;
 #[derive(OpenApi)]
 #[openapi(
     info(description = "Reverse Proxy K8s Api"),
+    tags(
+            (name = "health", description = "Health check endpoints.")
+        ),
 )]
 pub struct ApiDoc;
