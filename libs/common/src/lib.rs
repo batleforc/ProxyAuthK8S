c876e9864b880e63cf024f2678479a5f0130f7d7
Max Batleforc <maxleriche.60@gmail.com>
2025-10-12 16:27:42 +0200

    feat(redis): move from r2d2 to Deadpool, would be a headack perf wise

--- libs/common/src/lib.rs
+++ libs/common/src/lib.rs
@@ -1,32 +1,37 @@
 use std::env;
 
 use kube::Client;
-use r2d2::Pool;
-use redis::Client as RedisClient;
 use tracing::{info, instrument};
+
+use deadpool_redis::{Config, Pool, Runtime};
 
 #[derive(Clone)]
 pub struct State {
     pub client: Client,
-    pub redis_pool: Pool<RedisClient>,
+    redis: Pool,
 }
 
 impl State {
     #[instrument(name = "StateInit")]
     pub async fn new() -> Self {
-        let redis_client = RedisClient::open(
-            env::var("REDIS_URL").unwrap_or("redis://127.0.0.1:6379".to_string()),
-        )
-        .expect("failed to create redis Client");
-        let redis_pool = match Pool::builder().build(redis_client) {
-            Ok(p) => p,
-            Err(e) => panic!("failed to create redis Pool: {}", e),
-        };
+        let cfg =
+            Config::from_url(env::var("REDIS_URL").unwrap_or("redis://127.0.0.1:6379".to_string()));
+        let pool = cfg.create_pool(Some(Runtime::Tokio1)).unwrap();
         info!("Connected to Redis");
         let client = Client::try_default()
             .await
             .expect("failed to create kube Client");
         info!("Connected to Kubernetes");
-        Self { client, redis_pool }
+        Self {
+            client,
+            redis: pool,
+        }
+    }
+
+    #[instrument(name = "GetRedisConn", skip(self))]
+    pub async fn get_redis_conn(
+        &self,
+    ) -> Result<deadpool_redis::Connection, deadpool_redis::PoolError> {
+        self.redis.get().await
     }
 }
