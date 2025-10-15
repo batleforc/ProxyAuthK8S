bf53b78d9cd47e9c177bffb8acc7be59978fc400
Max Batleforc <maxleriche.60@gmail.com>
2025-10-16 00:00:01 +0200

    feat(crd): add oidc block

--- libs/crd/src/authentication_configuration/mod.rs
+++ libs/crd/src/authentication_configuration/mod.rs
@@ -2,6 +2,7 @@ pub mod claim_mappings;
 pub mod claim_validation_rules;
 pub mod issuer;
 pub mod user_validation_rule;
+use crate::default::default_disabled;
 
 use claim_mappings::ClaimMappings;
 use claim_validation_rules::ClaimValidationRule;
@@ -13,6 +14,7 @@ use user_validation_rule::UserValidationRule;
 #[derive(Serialize, Deserialize, Clone, Debug, JsonSchema)]
 pub struct AuthenticationConfiguration {
     pub jwt: Vec<JWTAuthenticator>,
+    pub oidc_provider: OidcProvider,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, JsonSchema)]
@@ -21,4 +23,14 @@ pub struct JWTAuthenticator {
     pub claim_validation_rules: Vec<ClaimValidationRule>,
     pub claim_mappings: ClaimMappings,
     pub user_validation_rules: Vec<UserValidationRule>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, JsonSchema)]
+pub struct OidcProvider {
+    #[serde(default = "default_disabled")]
+    pub enabled: bool,
+    pub issuer_url: String,
+    pub client_id: String,
+    pub client_secret: Option<String>,
+    pub extra_scope: String,
 }
