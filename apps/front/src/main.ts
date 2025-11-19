cfded201fdee6b92e3e80d3baf018e76f6b41cbf
Max Batleforc <maxleriche.60@gmail.com>
2025-11-19 23:20:18 +0100

    feat(front): add pinia

--- apps/front/src/main.ts
+++ apps/front/src/main.ts
@@ -1,8 +1,26 @@
 import './styles.scss';
 import router from './router';
-import { createApp } from 'vue';
+import type { Router } from 'vue-router';
+import { createPinia } from 'pinia';
+import { createApp, markRaw } from 'vue';
 import App from './app/App.vue';
+import {} from '@proxy-auth-k8s/front-api';
 
+const pinia = createPinia();
 const app = createApp(App);
+
+declare const RawSymbol: unique symbol;
+declare module 'pinia' {
+  export interface PiniaCustomProperties {
+    // by using a setter we can allow both strings and refs
+    router: Router & { [RawSymbol]?: true | undefined };
+    language: string;
+  }
+}
+pinia.use(({ store }) => {
+  store.router = markRaw(router);
+});
+
+app.use(pinia);
 app.use(router);
 app.mount('#root');
