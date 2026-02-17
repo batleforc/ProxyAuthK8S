99dbdade961931e05e6825479984e91ab621619d
Max Batleforc <maxleriche.60@gmail.com>
2026-02-17 21:45:01 +0100

    feat: wip

--- libs/server/crd/src/security/mod.rs
+++ libs/server/crd/src/security/mod.rs
@@ -15,6 +15,8 @@ pub struct SecurityConfiguration {
     /// Per group rate limiting configuration
     /// This take precedence over the global rate limiting configuration
     pub per_user_group_rate_limiting: Vec<PerUserGroupRateLimitingConfiguration>,
+    /// Allowed resources, limit the access to the proxy to only these resources, if empty all resources are allowed
+    pub allowed_ressources: Vec<AllowedPathConfigurationEnum>,
 }
 
 /// Pseudo Fail2Ban configuration
@@ -65,4 +67,75 @@ pub struct PerUserGroupRateLimitingConfiguration {
     pub max_requests_per_minute: u32,
     /// Claim to identify the user group, used in conjunction with claim_mappings in the JWTAuthenticator
     pub claim: String,
+}
+
+/// Enum of the allowed paths configuration, currently only supports path and crd, but can be extended in the future
+#[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
+pub enum AllowedPathConfigurationEnum {
+    Path(AllowedPathConfiguration),
+    Crd(AllowedCrdConfiguration),
+}
+
+/// Allowed path configuration, used in conjunction with the allowed_paths configuration
+#[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
+pub struct AllowedPathConfiguration {
+    /// The path to allow, if the request path starts with this path, it will be allowed
+    pub path: String,
+
+    /// Wether or not the ressource is namespaced, if true, the namespace access rules will be applied to this resource
+    pub namespace: NamespacedAccessConfiguration,
+    /// Namespaced or not
+    /// default: true
+    #[serde(default = "default_enabled")]
+    pub namespaced: bool,
+}
+
+/// Allowed crd configuration, used in conjunction with the allowed_paths configuration
+/// /apis/{group}/{version}/namespaces/{namespace}/{kind}/
+#[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
+pub struct AllowedCrdConfiguration {
+    /// The group of the crd
+    pub group: String,
+    /// The version of the crd
+    pub version: String,
+    /// The kind of the crd
+    pub kind: String,
+    /// Wether or not the kind has a specific plural form, if true, the plural form will be used in the path instead of the kind
+    /// for example, if the kind is "MyResource" and the plural form is "MyResources"
+    /// the path will be /apis/{group}/{version}/namespaces/{namespace}/myresources/
+    /// instead of       /apis/{group}/{version}/namespaces/{namespace}/myresource/
+    /// In case of cluster-scoped crd
+    /// the path will be /apis/{group}/{version}/myresources/
+    /// instead of       /apis/{group}/{version}/myresource/
+    pub plural: Option<String>,
+
+    /// Wether or not the ressource is namespaced, if true, the namespace access rules will be applied to this resource
+    pub namespace: NamespacedAccessConfiguration,
+    /// Namespaced or not
+    /// default: true
+    #[serde(default = "default_enabled")]
+    pub namespaced: bool,
+}
+
+/// Whether or not the rules restrict access to certains namespaces, if true, the allowed paths will be restricted to the namespaces specified in the allowed paths configuration
+#[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
+pub struct NamespacedAccessConfiguration {
+    /// If the feature is enabled
+    /// default: false
+    #[serde(default = "default_disabled")]
+    pub enabled: bool,
+
+    /// The kind of the namespace access rule
+    pub rule_kind: NamespacedAccessRuleKind,
+}
+
+/// Namespace rule kind, either :
+/// - A vec of allowed namespaces, if the user is only allowed to access a specific set of namespaces
+/// - A vec of denied namespaces, if the user is allowed to access all namespaces except a specific set of namespaces
+/// - A parametised rule, if the user is allowed to access namespaces that match a certain pattern, for example namespaces that start with "dev-{username}"
+#[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
+pub enum NamespacedAccessRuleKind {
+    AllowedNamespaces(Vec<String>),
+    DeniedNamespaces(Vec<String>),
+    ParametisedRule(String),
 }
