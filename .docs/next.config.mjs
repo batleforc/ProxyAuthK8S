1073b1124ed0357d683c2041e783cde2fb277e4f
Max Batleforc <maxleriche.60@gmail.com>
2026-03-08 23:23:17 +0100

    feat: add base path to doc

--- .docs/next.config.mjs
+++ .docs/next.config.mjs
@@ -7,6 +7,7 @@ const config = {
   serverExternalPackages: ['@takumi-rs/image-response'],
   output: 'export',
   reactStrictMode: true,
+  basePath: '/ProxyAuthK8S',
 };
 
 export default withMDX(config);
