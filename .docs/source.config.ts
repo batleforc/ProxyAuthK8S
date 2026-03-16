de3f538d7bde49f8ee7ee95edfde157d6bad8f4d
Max Batleforc <maxleriche.60@gmail.com>
2026-03-16 20:46:49 +0100

    feat: Mise en place d'une variable globales correspondant a la version de l'app

--- .docs/source.config.ts
+++ .docs/source.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
 import { metaSchema, pageSchema } from 'fumadocs-core/source/schema';
 import lastModified from 'fumadocs-mdx/plugins/last-modified';
 import { remarkMdxMermaid } from 'fumadocs-core/mdx-plugins';
+import { remarkVariables } from './lib/remark-variables.mjs';
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.dev/docs/mdx/collections
@@ -22,6 +23,6 @@ export default defineConfig({
   plugins: [lastModified()],
   mdxOptions: {
     // MDX options
-    remarkPlugins: [remarkMdxMermaid],
+    remarkPlugins: [remarkMdxMermaid, remarkVariables],
   },
 });
